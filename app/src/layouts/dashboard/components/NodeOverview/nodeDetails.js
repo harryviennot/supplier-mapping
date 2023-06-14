df4469ddb90d8cabb5dd863b4983b19a6fcc51b1
Harry Viennot <harry.viennot@epitech.eu>
2023-06-14 13:18:20 +0200

    update

--- app/src/layouts/dashboard/components/NodeOverview/nodeDetails.js
+++ app/src/layouts/dashboard/components/NodeOverview/nodeDetails.js
@@ -5,19 +5,34 @@ const NodeDetails = ({ node, onClose }) => {
 
   const risk = localStorage.getItem(`risk-${node.id}`);
   return (
-    <div style={{
-      border: '1px solid #ddd',
-      borderRadius: '10px',
-      padding: '10px',
-      marginTop: '15px',
-      backgroundColor: '#f9f9f9'
-    }}>
+    <div
+      style={{
+        border: "1px solid #ddd",
+        borderRadius: "10px",
+        padding: "10px",
+        marginTop: "15px",
+        backgroundColor: "#f9f9f9",
+      }}
+    >
       <h2>Node Details:</h2>
-      <p><strong>Name:</strong> {node.id}</p>
-      <p><strong>Commodities:</strong> {node.commodities.join(', ')}</p>
-      <p><strong>Risk:</strong> {risk}</p>
-      <p><strong>Location:</strong> {node.location.join(', ')}</p>
-      <button onClick={onClose} style={{ padding: '5px 10px', borderRadius: '5px', cursor: 'pointer'}}>Close</button>
+      <p>
+        <strong>Name:</strong> {node.id}
+      </p>
+      <p>
+        <strong>Commodities:</strong> {node.commodities.join(", ")}
+      </p>
+      <p>
+        <strong>Risk:</strong> {risk}
+      </p>
+      <p>
+        <strong>Location:</strong> {node.location.join(", ")}
+      </p>
+      <button
+        onClick={onClose}
+        style={{ padding: "5px 10px", borderRadius: "5px", cursor: "pointer" }}
+      >
+        Close
+      </button>
     </div>
   );
 };
